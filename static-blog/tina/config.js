6765ce3bef4f4d33b0c9138c7428aeea0efdd857
Hayley <haowens@email.unc.edu>
2023-06-20 10:35:16 -0400

    testing

--- static-blog/tina/config.js
+++ static-blog/tina/config.js
@@ -6,6 +6,8 @@ import collectionBlog from './collections/blog.js';
 import collectionArchive from './collections/archive.js';
 import collectionCategory from './collections/category.js'
 
+dotenv.config();
+
 const schema = defineSchema({
   collections: [
     collectionPage,
@@ -18,9 +20,9 @@ const schema = defineSchema({
 export const config = defineConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
   branch:
-    process.env.NEXT_PUBLIC_TINA_BRANCH || // custom branch env override
-    process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || // Vercel branch env
-    process.env.HEAD, // Netlify branch env
+    process.env.NEXT_PUBLIC_TINA_BRANCH, // || // custom branch env override
+    // process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || // Vercel branch env
+    // process.env.HEAD, // Netlify branch env
   token: process.env.TINA_TOKEN,
   media: {
     // If you wanted cloudinary do this
