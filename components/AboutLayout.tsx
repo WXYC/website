86eb3fc335138a72d9c74ddb21b98271b57e0064
camillacrane <102168879+camillacrane@users.noreply.github.com>
2023-07-17 11:41:55 -0400

    desktop About page side menu done

--- components/AboutLayout.tsx
+++ components/AboutLayout.tsx
@@ -8,29 +8,29 @@ const AboutLayout = ({children}) => {
 const currentRoute = useRouter().asPath;
  
   return (
-    <div className="w-5/6 mx-auto text-white flex">
+    <div className="md:w-5/6 w-full mx-auto text-white flex">
 
       {currentRoute === "/about" &&
-        <div className="flex flex-col">
-            <Link href="/about"><u>About</u></Link>
-            <Link href="/about/history">History</Link>
-            <Link href="/about/mission">Mission</Link>
+        <div className="flex flex-col w-1/5 text-xl bg-gradient-to-b from-neutral-900 to-black">
+            <div className="px-6 py-4 bg-neutral-700 hover:bg-neutral-500"><Link href="/about">About</Link></div>
+            <div className="px-6 py-4 hover:bg-neutral-500 text-neutral-400 hover:text-white"><Link href="/about/history">History</Link></div>
+            <div className="px-6 py-4 hover:bg-neutral-500 text-neutral-400 hover:text-white"><Link href="/about/mission">Mission</Link></div>
         </div>
       }
 
       {currentRoute === "/about/history" &&
-        <div className="flex flex-col">
-            <Link href="/about">About</Link>
-            <Link href="/about/history"><u>History</u></Link>
-            <Link href="/about/mission">Mission</Link>
+        <div className="flex flex-col w-1/5 text-xl bg-gradient-to-b from-neutral-900 to-black">
+            <div className="px-6 py-4 hover:bg-neutral-500 text-neutral-400 hover:text-white"><Link href="/about">About</Link></div>
+            <div className="px-6 py-4 bg-neutral-700 hover:bg-neutral-500"><Link href="/about/history">History</Link></div>
+            <div className="px-6 py-4 hover:bg-neutral-500 text-neutral-400 hover:text-white"><Link href="/about/mission">Mission</Link></div>
         </div>
       }
 
       {currentRoute === "/about/mission" &&
-        <div className="flex flex-col">
-            <Link href="/about">About</Link>
-            <Link href="/about/history">History</Link>
-            <Link href="/about/mission"><u>Mission</u></Link>
+        <div className="flex flex-col w-1/5 text-xl bg-gradient-to-b from-neutral-900 to-black">
+            <div className="px-6 py-4 hover:bg-neutral-500 text-neutral-400 hover:text-white"><Link href="/about">About</Link></div>
+            <div className="px-6 py-4 hover:bg-neutral-500 text-neutral-400 hover:text-white"><Link href="/about/history">History</Link></div>
+            <div className="px-6 py-4 bg-neutral-700 hover:bg-neutral-500"><Link href="/about/mission">Mission</Link></div>
         </div>
       }
       {children}
