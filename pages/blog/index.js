ee2e8af847e3f4e7ef1d7e125d532304e27466ef
camillacrane <102168879+camillacrane@users.noreply.github.com>
2023-08-07 16:50:33 -0400

    fixed mobile blog header

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -15,7 +15,7 @@ export default function PostList(props) {
       
       <div className="pt-3 md:pt-0 mx-auto h-32 md:h-24 items-center justify-between w-5/6 flex flex-col md:flex-row">
       <div className="kallisto text-5xl">WXYC PRESS</div>
-      <div className="  flex flex-row  h-1/2 md:h-1/2 justify-center md:justify-end  items-center w-full md:w-1/2 text-lg text-center">
+      <div className="  flex flex-row  h-1/2 relative z-50 md:h-1/2 justify-center md:justify-end  items-center w-full md:w-1/2 text-lg text-center">
                     <div className="hover:underline kallisto mx-2 md:mr-0 md:whitespace-nowrap   px-2 h-full flex flex-col justify-center " >
                         <a href="/blog/category/show-review">
                             Show Reviews
