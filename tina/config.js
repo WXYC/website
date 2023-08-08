d54c1308487c4a70a75b2c1e3f964be6fbacbede
Hayley <haowens@email.unc.edu>
2023-08-08 11:06:49 -0400

    testing again

--- tina/config.js
+++ tina/config.js
@@ -21,18 +21,9 @@ const schema = defineSchema({
 
 export const config = defineConfig({
   clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
-  branch:
-    process.env.NEXT_PUBLIC_TINA_BRANCH, // || // custom branch env override
-    // process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF || // Vercel branch env
-    // process.env.HEAD, // Netlify branch env
+  branch: process.env.NEXT_PUBLIC_TINA_BRANCH,
   token: process.env.TINA_TOKEN,
   media: {
-    // If you wanted cloudinary do this
-    // loadCustomStore: async () => {
-    //   const pack = await import("next-tinacms-cloudinary");
-    //   return pack.TinaCloudCloudinaryMediaStore;
-    // },
-    // this is the config for the tina cloud media store
     tina: {
       publicFolder: "public",
       mediaRoot: "uploads",
