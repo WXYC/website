0a3502e28ca5134b0293033d6173524ca8ba027e
Hayley <haowens@email.unc.edu>
2023-08-09 14:55:32 -0400

    please 2

--- tina/__generated__/client.js
+++ tina/__generated__/client.js
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: `https://content.tinajs.io/1.4/content/${process.env.TINA_PUBLIC_CLIENT_ID}/github/${process.env.NEXT_PUBLIC_TINA_BRANCH}`, token: process.env.TINA_TOKEN, queries });
+export const client = createClient({ url: `https://content.tinajs.io/1.4/content/${process.env.TINA_PUBLIC_CLIENT_ID}/github/main`, token: process.env.TINA_TOKEN, queries });
 export default client;
   
