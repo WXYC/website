c5dd40b33d2a5b8574ce5ccd713dd97f2d09e8c7
Hayley <haowens@email.unc.edu>
2023-08-09 14:11:27 -0400

    trying

---
+++ tina/__generated__/config.prebuild.jsx
@@ -0,0 +1,250 @@
+// tina/config.js
+import { defineConfig, defineSchema } from "tinacms";
+
+// tina/collections/page.js
+var page_default = {
+  label: "Page Content",
+  name: "page",
+  path: "content/page",
+  format: "mdx",
+  fields: [
+    {
+      name: "body",
+      label: "Main Content",
+      type: "rich-text",
+      isBody: true,
+      templates: [
+        {
+          name: "photoGallery",
+          label: "Photo Gallery",
+          fields: [
+            {
+              name: "photos",
+              label: "Photos",
+              type: "image",
+              list: true
+            }
+          ]
+        }
+      ]
+    }
+  ],
+  ui: {
+    router: ({ document }) => {
+      if (document._sys.filename === "home") {
+        return `/`;
+      }
+      return void 0;
+    }
+  }
+};
+
+// tina/collections/blog.js
+import { wrapFieldsWithMeta } from "tinacms";
+var blog_default = {
+  label: "Blog Posts",
+  name: "blog",
+  path: "content/blog",
+  format: "md",
+  ui: {
+    filename: {
+      // if disabled, the editor can not edit the filename
+      readonly: true,
+      // Example of using a custom slugify function
+      slugify: (values) => {
+        return `${values?.title?.toLowerCase().replace(/[^\w\s]/gi, "").replace(/\s+/g, "-")}`;
+      }
+    }
+  },
+  fields: [
+    {
+      type: "string",
+      label: "Title",
+      name: "title",
+      placeholder: "post title"
+    },
+    {
+      type: "string",
+      label: "Author",
+      name: "author",
+      placeholder: "post author"
+    },
+    {
+      name: "cover",
+      type: "image",
+      label: "Cover Image"
+    },
+    {
+      type: "object",
+      list: true,
+      name: "categories",
+      label: "Categories",
+      fields: [
+        {
+          type: "reference",
+          label: "Category",
+          name: "category",
+          collections: ["category"]
+        }
+      ]
+    },
+    {
+      type: "datetime",
+      label: "Published",
+      name: "published"
+    },
+    {
+      type: "string",
+      label: "Post Description",
+      name: "description",
+      placeholder: "post description"
+    },
+    {
+      type: "rich-text",
+      label: "Blog Post Body",
+      name: "body",
+      placeholder: "post body",
+      isBody: true
+      // ui: {
+      //   component: "textarea",
+      // },
+    }
+  ]
+  // ui: {
+  //   router: ({ document }) => {
+  //     return `/blog/${document._sys.filename}`;
+  //   },
+  // },
+};
+
+// tina/collections/archive.js
+var archive_default = {
+  label: "Archive",
+  name: "archive",
+  path: "content/archive",
+  format: "md",
+  ui: {
+    filename: {
+      // if disabled, the editor can not edit the filename
+      readonly: true,
+      // Example of using a custom slugify function
+      slugify: (values) => {
+        return `${values?.title?.toLowerCase().replace(/[^\w\s]/gi, "").replace(/\s+/g, "-")}`;
+      }
+    }
+  },
+  fields: [
+    {
+      type: "string",
+      label: "Title",
+      name: "title"
+    },
+    {
+      name: "cover",
+      type: "image",
+      label: "Cover Image"
+    },
+    {
+      type: "object",
+      list: true,
+      name: "categories",
+      label: "Categories",
+      fields: [
+        {
+          type: "reference",
+          label: "Category",
+          name: "category",
+          collections: ["category"]
+        }
+      ]
+    },
+    {
+      type: "datetime",
+      label: "Published",
+      name: "published"
+    },
+    {
+      type: "rich-text",
+      label: "Post Description",
+      name: "description",
+      isBody: true
+    }
+  ]
+};
+
+// tina/collections/category.js
+var category_default = {
+  label: "Category",
+  name: "category",
+  path: "content/category",
+  format: "md",
+  fields: [
+    {
+      type: "string",
+      label: "Title",
+      name: "title"
+    },
+    {
+      type: "boolean",
+      label: "Specialty Show",
+      name: "specialtyShow"
+    },
+    {
+      type: "string",
+      label: "Category Description",
+      name: "description",
+      placeholder: "Optional description"
+    }
+  ],
+  ui: {
+    filename: {
+      // if disabled, the editor can not edit the filename
+      readonly: true,
+      // Example of using a custom slugify function
+      slugify: (values) => {
+        return `${values?.title?.toLowerCase().replace(/[^\w\s]/gi, "").replace(/\s+/g, "-")}`;
+      }
+    }
+  }
+};
+
+// tina/config.js
+var schema = defineSchema({
+  collections: [
+    page_default,
+    blog_default,
+    archive_default,
+    category_default
+  ]
+});
+var config = defineConfig({
+  clientId: process.env.TINA_PUBLIC_CLIENT_ID,
+  branch: "main",
+  token: process.env.TINA_TOKEN,
+  media: {
+    tina: {
+      publicFolder: "public",
+      mediaRoot: "uploads"
+    }
+  },
+  search: {
+    tina: {
+      indexerToken: process.env.SEARCH_TOKEN,
+      stopwordLanguages: ["eng"]
+    },
+    indexBatchSize: 100,
+    maxSearchIndexFieldLength: 100
+  },
+  build: {
+    publicFolder: "public",
+    // The public asset folder for your framework
+    outputFolder: "admin"
+    // within the public folder
+  },
+  schema
+});
+var config_default = config;
+export {
+  config,
+  config_default as default
+};
---
+++ tina/__generated__/types.js
@@ -0,0 +1,345 @@
+export function gql(strings, ...args) {
+  let str = "";
+  strings.forEach((string, i) => {
+    str += string + (args[i] || "");
+  });
+  return str;
+}
+export const PagePartsFragmentDoc = gql`
+    fragment PageParts on Page {
+  body
+}
+    `;
+export const BlogPartsFragmentDoc = gql`
+    fragment BlogParts on Blog {
+  title
+  author
+  cover
+  categories {
+    __typename
+    category {
+      ... on Category {
+        title
+        specialtyShow
+        description
+      }
+      ... on Document {
+        _sys {
+          filename
+          basename
+          breadcrumbs
+          path
+          relativePath
+          extension
+        }
+        id
+      }
+    }
+  }
+  published
+  description
+  body
+}
+    `;
+export const ArchivePartsFragmentDoc = gql`
+    fragment ArchiveParts on Archive {
+  title
+  cover
+  categories {
+    __typename
+    category {
+      ... on Category {
+        title
+        specialtyShow
+        description
+      }
+      ... on Document {
+        _sys {
+          filename
+          basename
+          breadcrumbs
+          path
+          relativePath
+          extension
+        }
+        id
+      }
+    }
+  }
+  published
+  description
+}
+    `;
+export const CategoryPartsFragmentDoc = gql`
+    fragment CategoryParts on Category {
+  title
+  specialtyShow
+  description
+}
+    `;
+export const PageDocument = gql`
+    query page($relativePath: String!) {
+  page(relativePath: $relativePath) {
+    ... on Document {
+      _sys {
+        filename
+        basename
+        breadcrumbs
+        path
+        relativePath
+        extension
+      }
+      id
+    }
+    ...PageParts
+  }
+}
+    ${PagePartsFragmentDoc}`;
+export const PageConnectionDocument = gql`
+    query pageConnection($before: String, $after: String, $first: Float, $last: Float, $sort: String, $filter: PageFilter) {
+  pageConnection(
+    before: $before
+    after: $after
+    first: $first
+    last: $last
+    sort: $sort
+    filter: $filter
+  ) {
+    pageInfo {
+      hasPreviousPage
+      hasNextPage
+      startCursor
+      endCursor
+    }
+    totalCount
+    edges {
+      cursor
+      node {
+        ... on Document {
+          _sys {
+            filename
+            basename
+            breadcrumbs
+            path
+            relativePath
+            extension
+          }
+          id
+        }
+        ...PageParts
+      }
+    }
+  }
+}
+    ${PagePartsFragmentDoc}`;
+export const BlogDocument = gql`
+    query blog($relativePath: String!) {
+  blog(relativePath: $relativePath) {
+    ... on Document {
+      _sys {
+        filename
+        basename
+        breadcrumbs
+        path
+        relativePath
+        extension
+      }
+      id
+    }
+    ...BlogParts
+  }
+}
+    ${BlogPartsFragmentDoc}`;
+export const BlogConnectionDocument = gql`
+    query blogConnection($before: String, $after: String, $first: Float, $last: Float, $sort: String, $filter: BlogFilter) {
+  blogConnection(
+    before: $before
+    after: $after
+    first: $first
+    last: $last
+    sort: $sort
+    filter: $filter
+  ) {
+    pageInfo {
+      hasPreviousPage
+      hasNextPage
+      startCursor
+      endCursor
+    }
+    totalCount
+    edges {
+      cursor
+      node {
+        ... on Document {
+          _sys {
+            filename
+            basename
+            breadcrumbs
+            path
+            relativePath
+            extension
+          }
+          id
+        }
+        ...BlogParts
+      }
+    }
+  }
+}
+    ${BlogPartsFragmentDoc}`;
+export const ArchiveDocument = gql`
+    query archive($relativePath: String!) {
+  archive(relativePath: $relativePath) {
+    ... on Document {
+      _sys {
+        filename
+        basename
+        breadcrumbs
+        path
+        relativePath
+        extension
+      }
+      id
+    }
+    ...ArchiveParts
+  }
+}
+    ${ArchivePartsFragmentDoc}`;
+export const ArchiveConnectionDocument = gql`
+    query archiveConnection($before: String, $after: String, $first: Float, $last: Float, $sort: String, $filter: ArchiveFilter) {
+  archiveConnection(
+    before: $before
+    after: $after
+    first: $first
+    last: $last
+    sort: $sort
+    filter: $filter
+  ) {
+    pageInfo {
+      hasPreviousPage
+      hasNextPage
+      startCursor
+      endCursor
+    }
+    totalCount
+    edges {
+      cursor
+      node {
+        ... on Document {
+          _sys {
+            filename
+            basename
+            breadcrumbs
+            path
+            relativePath
+            extension
+          }
+          id
+        }
+        ...ArchiveParts
+      }
+    }
+  }
+}
+    ${ArchivePartsFragmentDoc}`;
+export const CategoryDocument = gql`
+    query category($relativePath: String!) {
+  category(relativePath: $relativePath) {
+    ... on Document {
+      _sys {
+        filename
+        basename
+        breadcrumbs
+        path
+        relativePath
+        extension
+      }
+      id
+    }
+    ...CategoryParts
+  }
+}
+    ${CategoryPartsFragmentDoc}`;
+export const CategoryConnectionDocument = gql`
+    query categoryConnection($before: String, $after: String, $first: Float, $last: Float, $sort: String, $filter: CategoryFilter) {
+  categoryConnection(
+    before: $before
+    after: $after
+    first: $first
+    last: $last
+    sort: $sort
+    filter: $filter
+  ) {
+    pageInfo {
+      hasPreviousPage
+      hasNextPage
+      startCursor
+      endCursor
+    }
+    totalCount
+    edges {
+      cursor
+      node {
+        ... on Document {
+          _sys {
+            filename
+            basename
+            breadcrumbs
+            path
+            relativePath
+            extension
+          }
+          id
+        }
+        ...CategoryParts
+      }
+    }
+  }
+}
+    ${CategoryPartsFragmentDoc}`;
+export function getSdk(requester) {
+  return {
+    page(variables, options) {
+      return requester(PageDocument, variables, options);
+    },
+    pageConnection(variables, options) {
+      return requester(PageConnectionDocument, variables, options);
+    },
+    blog(variables, options) {
+      return requester(BlogDocument, variables, options);
+    },
+    blogConnection(variables, options) {
+      return requester(BlogConnectionDocument, variables, options);
+    },
+    archive(variables, options) {
+      return requester(ArchiveDocument, variables, options);
+    },
+    archiveConnection(variables, options) {
+      return requester(ArchiveConnectionDocument, variables, options);
+    },
+    category(variables, options) {
+      return requester(CategoryDocument, variables, options);
+    },
+    categoryConnection(variables, options) {
+      return requester(CategoryConnectionDocument, variables, options);
+    }
+  };
+}
+import { createClient } from "tinacms/dist/client";
+const generateRequester = (client) => {
+  const requester = async (doc, vars, _options) => {
+    const data = await client.request({
+      query: doc,
+      variables: vars
+    });
+    return { data: data?.data, query: doc, variables: vars || {} };
+  };
+  return requester;
+};
+export const ExperimentalGetTinaClient = () => getSdk(
+  generateRequester(createClient({ url: "https://content.tinajs.io/1.4/content/cf0d2167-8e05-4026-ad42-d84b2ba25adb/github/main", queries }))
+);
+export const queries = (client) => {
+  const requester = generateRequester(client);
+  return getSdk(requester);
+};
