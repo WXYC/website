cbeef1801213ab697cdae7c60082aa9027a62f69
camillacrane <102168879+camillacrane@users.noreply.github.com>
2024-03-09 16:24:23 -0500

    download link in homepage banner

--- components/HomepageBanner.js
+++ components/HomepageBanner.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import {IoIosCloseCircle} from 'react-icons/io'
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 const Banner = ({children}) => {
 	const [isClosed, setIsClosed] = useState(false)
@@ -11,7 +12,7 @@ const Banner = ({children}) => {
 	return (
 		<div className="mx-auto px-1 mb-10 flex  h-24 md:h-18 lg:h-12 w-11/12 md:w-5/6 flex-row items-center justify-between rounded-full bg-neutral-800 lg:mb-1 ">
 			<p className="ml-5 w-4/5 md:w-full py-2 text-small md:text-xl lg:text-base">
-				NEW: The WXYC Android app is now available to download on Google Play!
+				NEW: The WXYC Android app is now available on Google Play! <a href="https://play.google.com/store/apps/details?id=org.wxyc.WXYCCH&pcampaignid=web_share" className="underline">Download now. </a> 
 			</p>
 			<button
 				className="mr-2 font-bold md:mr-5 lg:mr-10"
