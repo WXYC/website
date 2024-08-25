ea27c521c892af33b487b53074f13c402af8e502
camillacrane <102168879+camillacrane@users.noreply.github.com>
2024-08-25 12:24:39 -0400

    Index.js comments
    Added descriptive comments to index.js

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,9 @@ export default function Home(props) {
 			</div>
 
 			<div className="mx-auto flex w-5/6 flex-col gap-4">
-				<div className="-mt-5 flex w-full flex-col justify-center md:-mt-10 md:mr-10 lg:mt-5 ">
+				<div className="-mt-5 flex w-full flex-col justify-center md:-mt-10 md:mr-10 lg:mt-5">
+					
+					{/* Audio player on mobile site */}
 					<div className="mb-20 flex justify-center lg:hidden"> 
 						<AudioPlayerStream />
 						{/* <iframe src={`https://dj.wxyc.org/#/NowPlaying?theme=dark`} className="border-0 w-full h-[17.6rem] overflow-hidden mb-12 mt-16"/> */}
@@ -35,7 +37,7 @@ export default function Home(props) {
 					{/* if yes events: blog posts full row */}
 					{events.length > 0 && posts && <BlogCarouselFull posts={posts} />}
 
-					{/* "Submit a PSA" button on moblie */}
+					{/* "Submit a PSA" button on mobile */}
 					<div className="lg:hidden flex w-full justify-center"> 
 						<div className="mx-auto mt-10 flex h-16 w-4/6 md:w-2/6 flex-col items-center justify-center rounded-3xl bg-gradient-to-b from-neutral-200 to-neutral-400 text-xl text-black hover:text-neutral-700 lg:mx-0 ">
 							<div>
@@ -46,6 +48,7 @@ export default function Home(props) {
 						</div>
 					</div>
 
+					{/* Photo gallery */}
 					<div className="mx-auto mt-16 hidden w-5/6 items-center justify-center md:visible md:flex">
 						<PhotoGallery />
 					</div>
