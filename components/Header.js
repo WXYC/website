bc06bd8948f52861d1d1215ad64910bbb622db3c
camillacrane <102168879+camillacrane@users.noreply.github.com>
2025-07-17 13:41:47 -0400

    Move large logo to homepage
    Moved the large WXYC logo to index.js

--- components/Header.js
+++ components/Header.js
@@ -263,28 +263,7 @@ const Header = () => {
 						</div>
 					</div>
 				
-				{/* Header with WXYC logo lives here */}
-				<div className="mx-auto flex w-5/6 flex-col items-start justify-center pt-10 md:mb-10 md:pt-2 ">
-					<Link href="/">
-						{/* Header text parent container */}
-						<div className="mb-5 flex  w-full cursor-pointer flex-col items-center justify-center pt-20 md:flex-row md:items-end md:pt-20 lg:pt-1">
-							{/* Actual header text */}
-							<div className="flex w-full flex-col items-center justify-center md:w-3/4 md:pt-20 lg:w-2/5 lg:pt-1">
-								<Image src={photo} alt="Picture of the author" priority />
-								<h1 className=" kallistobold m-0 mx-auto text-6xl font-bold text-white no-underline">
-									89.3FM
-								</h1>
-								<div className="mt-2">
-									<h3 className="poppins mx-auto w-full text-center text-base md:mx-0  md:text-xl lg:text-base">
-										UNC-Chapel Hill&apos;s student-run, freeform radio station
-									</h3>
-								</div>
-							</div>
-						</div>
-					</Link>
-
-					
-				</div>
+				
 			</div>
 		</div>
 	)
