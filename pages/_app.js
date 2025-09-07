e32a92ceb5b728b25b21905304d989629dcd7dd8
camillacrane <102168879+camillacrane@users.noreply.github.com>
2025-09-07 14:32:45 -0400

    Fix header resizing bug
    Fixed bug where desktop header width would change between homepage and other pages

--- pages/_app.js
+++ pages/_app.js
@@ -29,7 +29,7 @@ const App = ({Component, pageProps}) => {
 	return (
 		<PostHogProvider client={posthog}>
 			<div className="flex flex-col lg:items-center">
-				<div className="w-92 m-0 flex h-full max-w-screen-2xl flex-col overflow-hidden bg-black font-poppins text-base text-white">
+				<div className="m-0 flex h-full w-full flex-col overflow-hidden bg-black font-poppins text-base text-white">
 					<Layout>
 						<Component {...pageProps} />
 					</Layout>
