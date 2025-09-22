e877e80fa63d478d803fe17a2aa602a9b1bb99c3
landmarco <marc.loeffke@gmail.com>
2025-09-22 13:34:58 -0400

    updates next.config to output static site to an /out directory

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
-module.exports = {
-	async rewrites() {
-		return [
-			{
-				source: '/admin',
-				destination: '/admin/index.html',
-			},
-		]
-	},
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export", // tells Next to emit static HTML in ./out
+  async rewrites() {
+    return [
+      {
+        source: "/admin",
+        destination: "/admin/index.html",
+      },
+    ]
+  },
+};
+
+module.exports = nextConfig;
