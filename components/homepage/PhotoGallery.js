0d6708b3b3350b416c9ec0875d20ec1093de2e4b
camillacrane <102168879+camillacrane@users.noreply.github.com>
2026-01-10 11:55:21 -0500

    photo gallery fix
    fix photos changing size in prod, smoother transitions

--- components/homepage/PhotoGallery.js
+++ components/homepage/PhotoGallery.js
@@ -1,5 +1,7 @@
-import React, {useState} from 'react'
-import {IoIosArrowDropright, IoIosArrowDropleft} from 'react-icons/io'
+import React, { useState } from 'react'
+import { IoIosArrowDropright, IoIosArrowDropleft } from 'react-icons/io'
+import Image from 'next/image'
+
 import greta from '../../images/greta.jpeg'
 import controlRoom from '../../images/controlroom.png'
 import cradleEvent from '../../images/cradleevent.jpeg'
@@ -7,59 +9,81 @@ import guyInStation from '../../images/guyinstation.jpeg'
 import flourescent from '../../images/flourescent.jpeg'
 import crowdSunglasses from '../../images/crowdsunglasses.jpeg'
 import kickballBench from '../../images/kickballbench.jpeg'
-import Image from 'next/image'
 
 // to add images to gallery:
 // (1) put in images directory
 // (2) import them above
 // (3) add to this images array
 const images = [
-	guyInStation,
-	greta,
-	flourescent,
-	crowdSunglasses,
-	kickballBench,
-	controlRoom,
-	cradleEvent,
+ flourescent,
+  guyInStation,
+  greta,
+  crowdSunglasses,
+  kickballBench,
+  controlRoom,
+  cradleEvent,
 ]
 
 export const PhotoGallery = () => {
-	const [currentIndex, setCurrentIndex] = useState(0)
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const [isFading, setIsFading] = useState(false)
 
-	const goToPrevious = () => {
-		setCurrentIndex((prevIndex) =>
-			prevIndex === 0 ? images.length - 1 : prevIndex - 1
-		)
-	}
+  const changeImage = (newIndex) => {
+    setIsFading(true)
 
-	const goToNext = () => {
-		setCurrentIndex((prevIndex) =>
-			prevIndex === images.length - 1 ? 0 : prevIndex + 1
-		)
-	}
+    setTimeout(() => {
+      setCurrentIndex(newIndex)
+      setIsFading(false)
+    }, 200)
+  }
 
-	return (
-		<div className="relative mb-10 flex w-full items-center">
-			<div className="h-full w-full object-cover">
-				<Image src={images[currentIndex]} alt="Gallery" sizes={'100vw'} />
-			</div>
+  const goToPrevious = () => {
+    const newIndex =
+      currentIndex === 0 ? images.length - 1 : currentIndex - 1
+    changeImage(newIndex)
+  }
 
-			<div className="arrows">
-				<button
-					onClick={goToPrevious}
-					className="absolute left-2 top-1/2 z-10 -translate-y-1/2 transform cursor-pointer text-2xl"
-				>
-					<IoIosArrowDropleft size={42} />
-				</button>
-				<button
-					onClick={goToNext}
-					className="absolute right-2 top-1/2 z-10 -translate-y-1/2 transform cursor-pointer text-2xl"
-				>
-					<IoIosArrowDropright size={42} />
-				</button>
-			</div>
-		</div>
-	)
+  const goToNext = () => {
+    const newIndex =
+      currentIndex === images.length - 1 ? 0 : currentIndex + 1
+    changeImage(newIndex)
+  }
+
+  return (
+    <div className="relative mb-10 w-full">
+      {/* Image Container */}
+      <div className="relative w-full aspect-[3/2] overflow-hidden rounded-lg">
+        <Image
+          src={images[currentIndex]}
+          alt="Gallery image"
+          fill
+          priority
+          sizes="100vw"
+          className={`object-cover transition-opacity duration-300 ease-in-out ${
+            isFading ? 'opacity-0' : 'opacity-100'
+          }`}
+        />
+      </div>
+
+      {/* Left Arrow */}
+      <button
+        onClick={goToPrevious}
+        aria-label="Previous image"
+        className="absolute left-2 top-1/2 z-10 -translate-y-1/2 rounded-full bg-black/40 p-1 text-white transition hover:bg-black/60"
+      >
+        <IoIosArrowDropleft size={42} />
+      </button>
+
+      {/* Right Arrow */}
+      <button
+        onClick={goToNext}
+        aria-label="Next image"
+        className="absolute right-2 top-1/2 z-10 -translate-y-1/2 rounded-full bg-black/40 p-1 text-white transition hover:bg-black/60"
+      >
+        <IoIosArrowDropright size={42} />
+      </button>
+    </div>
+  )
 }
 
 export default PhotoGallery
